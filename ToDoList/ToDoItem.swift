d6c53d8663cb8a55d12c525030faec5e8a35dc15
Ealden Esto E. Escañan <ealden@escanan.com>
2014-06-28 12:49:26 +0800

    ToDoItem model
    Guide says we should create an instance of NSObject.  I'm not sure if we
    still need to do that in Swift, so I went with a plain Swift class.

---
+++ ToDoList/ToDoItem.swift
@@ -0,0 +1,13 @@
+//
+//  ToDoItem.swift
+//  ToDoList
+//
+//  Created by Ealden Escañan on 6/28/14.
+//  Copyright (c) 2014 Ealden Escañan. All rights reserved.
+//
+
+import Foundation
+
+class ToDoItem {
+    
+}
